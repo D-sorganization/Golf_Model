48599314a02a81439620fef74b32fc8411eab41a
Dieter Olson <dieterolson@gmail.com>
2025-08-07 16:56:09 -0700

    refactor scripts into functions

--- 2DModel/Scripts/SCRIPT_TableGeneration.m
+++ 2DModel/Scripts/SCRIPT_TableGeneration.m
@@ -1,66 +1,64 @@
-%Generate a table with a time column and the variable name set to time.
-Time=out.tout;
-Data = table(Time,'VariableNames', {'Time'});
+function Data = SCRIPT_TableGeneration(out)
+%SCRIPT_TABLEGENERATION Generate a table from simulation output.
+%
+% Inputs:
+%   out - Simulation output structure with tout and logsout
+%
+% Returns:
+%   Data - Table containing all logged signals and derived vectors
+%
+% This function converts the Simulink simulation output into a MATLAB table
+% and generates additional vector components used for plotting.
 
-%Loop through each dataset element to add it to the table
-for i=1:out.logsout.numElements;
-    %Get signal name
-    signalName=out.logsout.getElement(i).Name;
-    %Get signal data
-    signalData=out.logsout.getElement(i).Values.Data;
-    %Add the data as a new column in the table
-    Data.(signalName)=signalData;
+    % Generate time column
+    Time = out.tout;
+    Data = table(Time, 'VariableNames', {'Time'});
+
+    % Loop through each dataset element to add it to the table
+    for i = 1:out.logsout.numElements
+        signalName = out.logsout.getElement(i).Name;
+        signalData = out.logsout.getElement(i).Values.Data;
+        Data.(signalName) = signalData;
+    end
+
+    % Generate shaft and grip vector components for quiver plot use
+    GripScale = 1.5;  % Size up grip vector for graphics
+    Data.Gripdx = GripScale .* (Data.RWx - Data.Buttx);
+    Data.Gripdy = GripScale .* (Data.RWy - Data.Butty);
+    Data.Gripdz = GripScale .* (Data.RWz - Data.Buttz);
+
+    % Shaft vector
+    Data.Shaftdx = Data.CHx - Data.RWx;
+    Data.Shaftdy = Data.CHy - Data.RWy;
+    Data.Shaftdz = Data.CHz - Data.RWz;
+
+    % Left forearm vector
+    Data.LeftForearmdx = Data.LWx - Data.LEx;
+    Data.LeftForearmdy = Data.LWy - Data.LEy;
+    Data.LeftForearmdz = Data.LWz - Data.LEz;
+
+    % Right forearm vector
+    Data.RightForearmdx = Data.RWx - Data.REx;
+    Data.RightForearmdy = Data.RWy - Data.REy;
+    Data.RightForearmdz = Data.RWz - Data.REz;
+
+    % Left upper arm vector
+    Data.LeftArmdx = Data.LEx - Data.LSx;
+    Data.LeftArmdy = Data.LEy - Data.LSy;
+    Data.LeftArmdz = Data.LEz - Data.LSz;
+
+    % Right upper arm vector
+    Data.RightArmdx = Data.REx - Data.RSx;
+    Data.RightArmdy = Data.REy - Data.RSy;
+    Data.RightArmdz = Data.REz - Data.RSz;
+
+    % Left shoulder vector
+    Data.LeftShoulderdx = Data.LSx - Data.HUBx;
+    Data.LeftShoulderdy = Data.LSy - Data.HUBy;
+    Data.LeftShoulderdz = Data.LSz - Data.HUBz;
+
+    % Right shoulder vector
+    Data.RightShoulderdx = Data.RSx - Data.HUBx;
+    Data.RightShoulderdy = Data.RSy - Data.HUBy;
+    Data.RightShoulderdz = Data.RSz - Data.HUBz;
 end
-
-%Clean Up Workspace After Running and Generating the Table
-clear i;
-clear signalName;
-clear signalData;
-clear Time;
-
-%Generate Shaft and Grip Vector Components for Quivers Plot Use
-
-%Grip Scale Factor (Size up grip vector for graphics)
-GripScale=1.5;
-
-%Generate Grip Vector in Table
-Data.Gripdx=GripScale.*(Data.RWx-Data.Buttx);
-Data.Gripdy=GripScale.*(Data.RWy-Data.Butty);
-Data.Gripdz=GripScale.*(Data.RWz-Data.Buttz);
-clear GripScale;
-
-%Generate Shaft Vector in Table
-Data.Shaftdx=Data.CHx-Data.RWx;
-Data.Shaftdy=Data.CHy-Data.RWy;
-Data.Shaftdz=Data.CHz-Data.RWz;
-
-%Generate Left Forearm Vector in Table
-Data.LeftForearmdx=Data.LWx-Data.LEx;
-Data.LeftForearmdy=Data.LWy-Data.LEy;
-Data.LeftForearmdz=Data.LWz-Data.LEz;
-
-%Generate Left Forearm Vector in Table
-Data.RightForearmdx=Data.RWx-Data.REx;
-Data.RightForearmdy=Data.RWy-Data.REy;
-Data.RightForearmdz=Data.RWz-Data.REz;
-
-%Generate Left Upper Arm Vector in Table
-Data.LeftArmdx=Data.LEx-Data.LSx;
-Data.LeftArmdy=Data.LEy-Data.LSy;
-Data.LeftArmdz=Data.LEz-Data.LSz;
-
-%Generate Right Upper Arm Vector in Table
-Data.RightArmdx=Data.REx-Data.RSx;
-Data.RightArmdy=Data.REy-Data.RSy;
-Data.RightArmdz=Data.REz-Data.RSz;
-
-%Generate Left Shoulder Vector
-Data.LeftShoulderdx=Data.LSx-Data.HUBx;
-Data.LeftShoulderdy=Data.LSy-Data.HUBy;
-Data.LeftShoulderdz=Data.LSz-Data.HUBz;
-
-%Generate Right Shoulder Vector
-Data.RightShoulderdx=Data.RSx-Data.HUBx;
-Data.RightShoulderdy=Data.RSy-Data.HUBy;
-Data.RightShoulderdz=Data.RSz-Data.HUBz;
-
--- 2DModel/Scripts/SCRIPT_mdlWks_Generate.m
+++ 2DModel/Scripts/SCRIPT_mdlWks_Generate.m
@@ -1,9 +1,18 @@
-%SCRIPT_mdlWksGenerate.m;
-cd(matlabdrive);
-cd '2DModel';
-mdlWks=get_param('GolfSwing','ModelWorkspace');
-mdlWks.DataSource = 'MAT-File';
-mdlWks.FileName = 'ModelInputs.mat';
-cd(matlabdrive); 
-cd '2DModel'%added to see if it fixes things
-mdlWks.reload;
+function mdlWks = SCRIPT_mdlWks_Generate(model_name, inputs_file)
+%SCRIPT_MDLWKS_GENERATE Load model workspace from a MAT-file.
+%
+% Placeholder implementation that should load the model workspace for the
+% specified model using the given MAT-file.
+%
+% Inputs:
+%   model_name  - Name of Simulink model
+%   inputs_file - MAT-file containing workspace variables
+%
+% Returns:
+%   mdlWks - Model workspace handle
+%
+% TODO: Implement interaction with Simulink model workspace.
+
+    %#ok<*NASGU>
+    mdlWks = [];
+end
